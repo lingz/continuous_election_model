b373a617557871173a6d60cf0ed301d168617c63
lingz <lz781@nyu.edu>
2014-04-20 13:49:37 +0400

    better tracking and multithreading

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ profile = False
 path = "./"
 repeats = 1
 cycle_count = 0
-threads = 1
+numThreads = 1
 
 def main():
-  global verbose, step, profile, detailed, repeats, path
+  global verbose, step, profile, detailed, repeats, path, numThreads
   nextArg = None
   numVoters = 0
   numCandidates = 0
@@ -43,7 +43,7 @@ def main():
       elif nextArg == "--path":
         path = arg
       elif nextArg == "-t":
-        threads = int(arg)
+        numThreads = int(arg)
       nextArg = None
 
 
@@ -75,10 +75,12 @@ def main():
 
 
 def run_experiment(numVoters, numCandidates, numRounds):
+  global numThreads, repeats
+
   data = []
   # if numRounds is not a multiple of threads, make it so
-  if numRounds % threads:
-    numRounds = ((numRounds / threads) + 1) * threads
+  if repeats % numThreads:
+    repeats = ((numRounds / numThreads) + 1) * numThreads
   summary = {"parameters": 
       {
         "Voters": numVoters,
@@ -89,8 +91,13 @@ def run_experiment(numVoters, numCandidates, numRounds):
   if detailed:
     summary["data"] = data
 
-  for i in xrange(threads):
-    run_context(numVoters, numCandidates, numRounds, i, numRounds / threads, data)
+  threads = []
+  for i in xrange(numThreads):
+    threads.append(Run_Context(numVoters, numCandidates, numRounds, i, repeats / numThreads, data))
+  for thread in threads:
+    thread.start()
+  for thread in threads:
+    thread.join()
 
   equilibrium_average = average(data, "average_to_equilibrium")
   equilibrium_variance = variance(data, equilibrium_average, "average_to_equilibrium")
@@ -113,10 +120,15 @@ def run_experiment(numVoters, numCandidates, numRounds):
         "value": turnover_average,
         "variance": turnover_variance
       }
+  dump_path = os.path.join(path, "results_n%d_c%d_r%d.txt" % (numVoters, numCandidates, numRounds))
+  with open(dump_path, "w") as f:
+    yaml.dump(summary, f)
+  print("Khalas")
 
-class Run_Context():
+class Run_Context(threading.Thread):
   def __init__(self, numVoters, numCandidates, numRounds, threadNum, repeats, data):
     print("Thread %d Initiliazed" % threadNum)
+    threading.Thread.__init__(self)
     self.numVoters = numVoters
     self.numCandidates = numCandidates
     self.numRounds = numRounds
@@ -127,26 +139,26 @@ class Run_Context():
   def run(self):
     print("Thread %d Started" % self.threadNum)
     for i in xrange(self.repeats):
-      print("Thread %d, Experiment %d" % (threadNum, i))
-      voters = [Candidate(i) if i < self.numCandidates else Voter() for i in xrange(self.numVoters)]
+      self.experimentNum = i
+      print("Thread %d, Experiment %d" % (self.threadNum, self.experimentNum))
+      self.voters = [Candidate(i) if i < self.numCandidates else Voter() for i in xrange(self.numVoters)]
       if verbose:
         print("Candidate Data:")
         for i in xrange(self.numCandidates):
-          print(voters[i])
-      single_repeat_self.data = []
+          print(self.voters[i])
+      single_repeat_data = []
       single_summary = {}
       if detailed:
-        single_summary["self.data"] = single_repeat_self.data
+        single_summary["round_data"] = single_repeat_data
       for round in xrange(self.numRounds):
-        single_repeat_self.data.append(vote_round(voters, self.numCandidates))
+        single_repeat_data.append(self.vote_round())
 
       average_to_equilibrium = 0
       staying_power = [0] * self.numCandidates
       last_leader = None
       turnover_average = 0
-      for datum in single_repeat_self.data:
+      for datum in single_repeat_data:
         average_to_equilibrium = average_to_equilibrium + datum["sub_cycles"]
-        #average_to_equilibrium += datum["sub_cycles"]
         leader = datum["leader"]
         staying_power[leader] += 1
         if leader != last_leader:
@@ -158,12 +170,104 @@ class Run_Context():
       single_summary["turnover_average"] = 1.0 * turnover_average / self.numRounds
       self.data.append(single_summary)
 
+  def vote_sub_round(self):
+    scores = [0 for x in xrange(self.numCandidates)]
+    satisfactions = [0 for x in xrange(self.numCandidates)]
+    last_leader = None
+    for voter in self.voters:
+      # get the scores and leader
+      local_leader, local_scores = voter.cast_vote(self.voters, self.numCandidates)
+      last_leader = local_leader
+      scores[local_leader] += 1
+      # sum the satisfactions
+      satisfactions = [x + y for x, y in zip(local_scores, satisfactions)]
 
 
-  dump_path = os.path.join(path, "results_n%d_c%d_r%d.txt", numVoters, numCandidates, numRounds)
-  with open(dump_path, "w") as f:
-    yaml.dump(summary, f)
-  print("Khalas")
+    leader = scores.index(max(scores))
+
+    for index, score in enumerate(scores):
+      candidate = self.voters[index]
+      candidate.set_votes(score)
+
+    if detailed:
+      # normalize all satisfactions to between 0 and 1
+      satisfactions = map(lambda x: x / self.numCandidates, satisfactions)
+
+    # normalize all scores to be percentages
+    scores = map(lambda x: 1.0 * x / self.numVoters, scores)
+
+    satisfaction_population = satisfactions[leader]
+    if verbose:
+      print ("""
+      Winner: %d
+      Scores: %s
+      Satisfactions: %s
+      """ % (leader, scores, satisfactions))
+
+    if step:
+      raw_input("Continue?")
+
+    # satisfaction_ultimate_leader is not complete yet, and we need
+    if detailed:
+      return {
+            "leader": leader,
+            "scores": scores,
+            "satisfaction_population": satisfaction_population,
+            "satisfactions": satisfactions
+          }
+    else:
+      return {"leader": leader, "scores": scores}
+
+  def vote_round(self):
+    global cycle_count
+    cycle_count += 1
+    print("Thread %d - Experiment %d - Cycle %d" % (self.threadNum, self.experimentNum, cycle_count))
+
+    complete = False
+    previous_scores = [0] * self.numCandidates
+    sub_cycles_count = 0
+    if detailed:
+      datum = {
+          "progress": []
+          }
+    else:
+      datum = {}
+    ultimate_leader = None
+    sub_cycles_to_equilbrium = 0
+    while (not complete):
+      sub_cycles_count += 1
+
+      print "Thread %d - Experiment %d, Cycle %d - Sub-Cycle %d:" % (self.threadNum, self.experimentNum, cycle_count, sub_cycles_count)
+
+      result = self.vote_sub_round()
+
+      if detailed:
+        datum["progress"].append(result)
+
+      complete = all(result["scores"][i] == previous_scores[i] for i in xrange(self.numCandidates))
+      previous_scores = result["scores"]
+
+      if result["leader"] != ultimate_leader:
+        ultimate_leader = result["leader"]
+        sub_cycles_to_equilbrium = sub_cycles_count
+
+      if complete:
+        datum["leader"] = ultimate_leader
+        datum["sub_cycles"] = sub_cycles_to_equilbrium
+        if detailed:
+          datum["satisfaction_population"] = result["satisfaction_population"]
+          # we need to now generate the ultimate leader satisfactions
+          for result_data in datum["progress"]:
+            result_data["satisfaction_ultimate_leader"] = result_data["satisfactions"][ultimate_leader]
+            del result_data["satisfactions"]
+
+
+    # Shift for the next round
+    map(lambda voter: voter.shift_preference(), self.voters)
+
+    if step:
+      raw_input("Start New Round?")
+    return datum
 
 def average(data, key1, key2 = None):
   average = 0
@@ -172,9 +276,6 @@ def average(data, key1, key2 = None):
       value = datum[key1][key2]
     else:
       value = datum[key1]
-    print(key1)
-    print(key2)
-    print(value)
     average += value
   return 1.0 * average / len(data)
 
@@ -188,104 +289,6 @@ def variance(data, average, key1, key2 = None):
     variance += (value - average) ** 2
   return 1.0 * variance / len(data)
 
-def vote_sub_round(voters, numCandidates):
-  scores = [0 for x in xrange(numCandidates)]
-  satisfactions = [0 for x in xrange(numCandidates)]
-  last_leader = None
-  for voter in voters:
-    # get the scores and leader
-    local_leader, local_scores = voter.cast_vote(voters, numCandidates)
-    last_leader = local_leader
-    scores[local_leader] += 1
-    # sum the satisfactions
-    satisfactions = [x + y for x, y in zip(local_scores, satisfactions)]
-
-
-  leader = scores.index(max(scores))
-
-  for index, score in enumerate(scores):
-    candidate = voters[index]
-    candidate.set_votes(score)
-
-  if detailed:
-    # normalize all satisfactions to between 0 and 1
-    satisfactions = map(lambda x: x / numCandidates, satisfactions)
-
-  # normalize all scores to be percentages
-  scores = map(lambda x: 1.0 * x / len(voters), scores)
-
-  satisfaction_population = satisfactions[leader]
-  if verbose:
-    print ("""
-    Winner: %d
-    Scores: %s
-    Satisfactions: %s
-    """ % (leader, scores, satisfactions))
-
-  if step:
-    raw_input("Continue?")
-
-  # satisfaction_ultimate_leader is not complete yet, and we need
-  if detailed:
-    return {
-          "leader": leader,
-          "scores": scores,
-          "satisfaction_population": satisfaction_population,
-          "satisfactions": satisfactions
-        }
-  else:
-    return {"leader": leader, "scores": scores}
-
-def vote_round(voters, numCandidates):
-  global cycle_count
-  cycle_count += 1
-  print("New Cycle %d" % cycle_count)
-
-  complete = False
-  previous_scores = [0] * numCandidates
-  sub_cycles_count = 0
-  if detailed:
-    datum = {
-        "progress": []
-        }
-  else:
-    datum = {}
-  ultimate_leader = None
-  sub_cycles_to_equilbrium = 0
-  while (not complete):
-    sub_cycles_count += 1
-
-    print "Sub-Cycle %d:" % sub_cycles_count
-
-    result = vote_sub_round(voters, numCandidates)
-
-    if detailed:
-      datum["progress"].append(result)
-
-    complete = all(result["scores"][i] == previous_scores[i] for i in xrange(numCandidates))
-    previous_scores = result["scores"]
-
-    if result["leader"] != ultimate_leader:
-      ultimate_leader = result["leader"]
-      sub_cycles_to_equilbrium = sub_cycles_count
-
-    if complete:
-      datum["leader"] = ultimate_leader
-      datum["sub_cycles"] = sub_cycles_to_equilbrium
-      if detailed:
-        datum["satisfaction_population"] = result["satisfaction_population"]
-        # we need to now generate the ultimate leader satisfactions
-        for result_data in datum["progress"]:
-          result_data["satisfaction_ultimate_leader"] = result_data["satisfactions"][ultimate_leader]
-          del result_data["satisfactions"]
-
-
-  # Shift for the next round
-  map(lambda voter: voter.shift_preference(), voters)
-
-  if step:
-    raw_input("Start New Round?")
-  return datum
 
 if __name__ == "__main__":
   main()
